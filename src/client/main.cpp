2746c19b8ec072ef040820a19bc27c132641e914
SrPatsu21 <paulosergiopierdona@gmail.com>
2026-03-10 22:03:33 -0300

    lfix

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -11,31 +11,31 @@ int main() {
     // World
     hittable_list world;
 
-    // world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(0, -50.5, -1), 50));
-    world.add(make_shared<triangle>(
-        point3(-1, 1, -3),
-        point3(0, 2, -3),
-        point3(1, 1, -3)
-    ));
+    // world.add(make_shared<triangle>(
+    //     point3(-1, 1, -3),
+    //     point3(0, 2, -3),
+    //     point3(1, 1, -3)
+    // ));
 
-    world.add(make_shared<triangle>(
-            point3(-1, -1, -3),
-            point3(1, -1, -3),
-            point3(1, 1, -3)
-        )
-    );
-    world.add(make_shared<triangle>(
-            point3(-1, -1, -3),
-            point3(1, 1, -3),
-            point3(-1, 1, -3)
-        )
-    );
+    // world.add(make_shared<triangle>(
+    //         point3(-1, -1, -3),
+    //         point3(1, -1, -3),
+    //         point3(1, 1, -3)
+    //     )
+    // );
+    // world.add(make_shared<triangle>(
+    //         point3(-1, -1, -3),
+    //         point3(1, 1, -3),
+    //         point3(-1, 1, -3)
+    //     )
+    // );
 
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
+    cam.image_width = 800;
     cam.samples_per_pixel = 16;
     cam.max_depth = 10;
 
